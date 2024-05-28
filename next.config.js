452a3ff565131f1ac17a9756ab2a27d6e831775e
shreyakhare013 <140873760+shreyakhare013@users.noreply.github.com>
2024-05-28 22:19:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,32 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   eslint:{
+//     ignoreDuringBuilds:true,
+//   }
+// }
+
+// module.exports = nextConfig
 const nextConfig = {
   reactStrictMode: true,
-  eslint:{
-    ignoreDuringBuilds:true,
-  }
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    // Check if it's the server build, as `self` is not available in Node.js
+    if (!isServer) {
+      // Define `self` as `global` if it's not already defined
+      config.plugins.push(
+        new (require('webpack').DefinePlugin)({
+          'self': 'global'
+        })
+      );
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+
+
